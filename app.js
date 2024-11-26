16116ef41fe3fa92ff6b77e8e99794df45303617
OrganicaHuancavelica <Charlypedidovirtual@gmail.com>
2024-11-25 22:52:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ const productosPorCategoria = {
         { nombre: 'conazo', precio: 5.00, imagen:'./images/helados/conazo.png'  },
     ],
     'bebidas-frias': [
-        { nombre: 'Café Americano F', precio: 7.50, imagen:'./images/bebidasCalientes/CafeAmericanoo.png'},
-        { nombre: 'Orange Americano ', precio: 9.00, imagen:'./images/bebidasFrias/Aranangee.png'},
-        { nombre: 'Capuccino F', precio: 9.50, imagen:'./images/bebidasCalientes/capucchinoo.png'},
-        { nombre: 'Café Mocha F', precio: 9.50, imagen:'./images/bebidasCalientes/CafeMocchaa.png'},
+        { nombre: 'Café Americano F', precio: 7.50, imagen:'./images/bebidasCalientes/duo-chocovaso.jpeg'},
+        { nombre: 'Orange Americano ', precio: 9.00, imagen:'./images/bebidasFrias/duo-cuchareable.jpeg'},
+        { nombre: 'Capuccino F', precio: 9.50, imagen:'./images/bebidasCalientes/duo-pink-drink.jpeg'},
+        { nombre: 'Café Mocha F', precio: 9.50, imagen:'./images/bebidasCalientes/duo-fresas-vainilla.jpeg'},
         { nombre: 'Rosa y Chocolate', precio: 10.50, imagen:'./images/bebidasFrias/rosa.png'},
         { nombre: 'Matcha Milk', precio: 12.50, imagen:'./images/bebidasFrias/matchaa.png'},
         { nombre: 'Leche c/ chocolat', precio: 9.50 , imagen:'./images/bebidasFrias/leche.png'}
