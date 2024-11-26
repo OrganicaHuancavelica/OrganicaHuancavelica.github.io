59f2a3f61fd1b1c3540c5657cf0e18c51bf91a24
OrganicaHuancavelica <Charlypedidovirtual@gmail.com>
2024-11-25 23:10:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -38,19 +38,19 @@ const productosPorCategoria = {
         { nombre: 'conazo', precio: 5.00, imagen:'./images/helados/conazo.png'  },
     ],
     'bebidas-frias': [
-        { nombre: 'Café Americano F', precio: 7.50, imagen:'./images/bebidasCalientes/duo-chocovaso.jpeg'},
-        { nombre: 'Orange Americano ', precio: 9.00, imagen:'./images/bebidasFrias/duo-cuchareable.jpeg'},
-        { nombre: 'Capuccino F', precio: 9.50, imagen:'./images/bebidasCalientes/duo-pink-drink.jpeg'},
-        { nombre: 'Café Mocha F', precio: 9.50, imagen:'./images/bebidasCalientes/duo-fresas-vainilla.jpeg'},
+        { nombre: 'Café Americano F', precio: 7.50, imagen:'./images/bebidasCalientes/CafeAmericano.png'},
+        { nombre: 'Orange Americano ', precio: 9.00, imagen:'./images/bebidasFrias/Aranangee.png'},
+        { nombre: 'Capuccino F', precio: 9.50, imagen:'./images/bebidasCalientes/capucchinoo.png'},
+        { nombre: 'Café Mocha F', precio: 9.50, imagen:'./images/bebidasCalientes/CafeMocchaa.png'},
         { nombre: 'Rosa y Chocolate', precio: 10.50, imagen:'./images/bebidasFrias/rosa.png'},
         { nombre: 'Matcha Milk', precio: 12.50, imagen:'./images/bebidasFrias/matchaa.png'},
         { nombre: 'Leche c/ chocolat', precio: 9.50 , imagen:'./images/bebidasFrias/leche.png'}
     ],
     'packs-desayuno': [
-        { nombre: 'Café Americano F', precio: 25.50, imagen:'./images/bebidasCalientes/CafeAmericanoo.png'},
-        { nombre: 'Orange Americano ', precio: 9.00, imagen:'./images/bebidasFrias/Aranangee.png'},
-        { nombre: 'Capuccino F', precio: 9.50, imagen:'./images/bebidasCalientes/capucchinoo.png'},
-        { nombre: 'Café Mocha F', precio: 9.50, imagen:'./images/bebidasCalientes/CafeMocchaa.png'},
+        { nombre: 'Café Americano F', precio: 25.50, imagen:'./images/bebidasCalientes/duo-chocovaso.jpeg'},
+        { nombre: 'Orange Americano ', precio: 9.00, imagen:'./images/bebidasFrias/duo-cuchareable.jpeg'},
+        { nombre: 'Capuccino F', precio: 9.50, imagen:'./images/bebidasCalientes/duo-fresas-vainilla.jpeg'},
+        { nombre: 'Café Mocha F', precio: 9.50, imagen:'./images/bebidasCalientes/duo-pink-drink.jpeg'},
         { nombre: 'Rosa y Chocolate', precio: 10.50, imagen:'./images/bebidasFrias/rosa.png'},
         { nombre: 'Matcha Milk', precio: 12.50, imagen:'./images/bebidasFrias/matchaa.png'},
         { nombre: 'Leche c/ chocolat', precio: 9.50 , imagen:'./images/bebidasFrias/leche.png'}
