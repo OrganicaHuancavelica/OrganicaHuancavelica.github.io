fa3b434f25df8c0061fc3558b85f95346eaad1de
Ferch1 <ferdesbe21@gmail.com>
2025-02-11 17:58:13 -0500

    actualización de imagenes y js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // Productos por categoría
 const productosPorCategoria = {
     'bebidas-calientes': [
-        { nombre: 'Café Americano', precio: 3.00,imagen: './images/bebidasCalientes/cafe-caliente.jpg' },
-        { nombre: 'Capuccino', precio: 5.50,imagen: './images/bebidasCalientes/capuccino-caliente.jpg' },
-        { nombre: 'Café Moccha', precio: 6.00,imagen: './images/bebidasCalientes/cafe-mocha.jpg' },
-        { nombre: 'Café Bombón', precio: 6.00,imagen: './images/bebidasCalientes/CafeBombonn.png' },
-        { nombre: 'Café Caramel', precio: 6.00,imagen: './images/bebidasCalientes/caramel-caliente.jpg' },
-        { nombre: 'Leche con chocolate', precio: 5.50,imagen: './images/bebidasCalientes/leche-chocolate-caliente.jpg' },
-        { nombre: 'Mate', precio: 3.00,imagen:'./images/bebidasCalientes/mate-caliente.jpg' },
-        { nombre: 'Leche', precio: 5.00,imagen:'./images/bebidasCalientes/leche-caliente.webp' },
+        { nombre: 'Café Americano', precio: 4.00,imagen: './images/bebidasCalientes/Cafe-Americano.jpeg' },
+        { nombre: 'Capuccino', precio: 6.00,imagen: './images/bebidasCalientes/Capuccino.jpeg' },
+        { nombre: 'Café Moccha', precio: 7.00,imagen: './images/bebidasCalientes/Moccha-Caliente.jpeg' },
+        { nombre: 'Café Bombón', precio: 7.00,imagen: './images/bebidasCalientes/Cafe-Bombon.jpeg' },
+        { nombre: 'Café Caramel', precio: 7.00,imagen: './images/bebidasCalientes/Cafe-Caramel.jpeg' },
+        { nombre: 'Submarino', precio: 7.50,imagen: './images/bebidasCalientes/Submarino.jpeg' },
+        { nombre: 'Chocolate', precio: 5.50,imagen:'./images/bebidasCalientes/Chocolate.jpg' },
+        { nombre: 'Mate', precio: 3.00,imagen:'./images/bebidasCalientes/Mate.jpg' },
+        { nombre: 'Leche', precio: 5.00,imagen:'./images/bebidasCalientes/Leche.jpeg' },
     ],
     'limonadas': [
         { nombre: 'Cherry', precio: 5.50,imagen: './images/limonadas/l-limonada-cherry.jpg' },
@@ -16,49 +17,62 @@ const productosPorCategoria = {
         { nombre: 'Menta', precio: 5.50,imagen: './images/limonadas/l-menta.jpg' },
         { nombre: 'Tradicional', precio: 5.50,imagen: './images/limonadas/l-limonada.jpg' },
     ],
-    'shakes': [
-        { nombre: 'Capuccino', precio: 9.50, imagen:'./images/shakes/capuccino.png' },
-        { nombre: 'Oreo', precio: 9.50, imagen:'./images/shakes/oreo-shake.png'  },
-        { nombre: 'Chocolate', precio: 9.50, imagen:'./images/shakes/chocolate-shake.png'  },
-        { nombre: 'Pink Drink', precio: 9.50, imagen:'./images/shakes/pink-drink.png'  },
-        { nombre: 'Caramel Café', precio: 9.50, imagen:'./images/shakes/caramel-cafe.png'  },
-        { nombre: 'Menta', precio: 9.50, imagen:'./images/shakes/menta.png'  },
-        { nombre: 'Chocovaso-adicional', precio: 2.00, imagen:'./images/shakes/choco-vaso.jpeg'  },
+    'frappe': [
+        { nombre: 'Capuccino', precio: 9.50, imagen:'./images/Frappe/capuccino.png' },
+        { nombre: 'Oreo', precio: 9.50, imagen:'./images/Frappe/oreo-shake.png'  },
+        { nombre: 'Chocolate', precio: 9.50, imagen:'./images/Frappe/chocolate-shake.png'  },
+        { nombre: 'Pink Drink', precio: 9.50, imagen:'./images/Frappe/pink-drink.png'  },
+        { nombre: 'Caramel Café', precio: 9.50, imagen:'./images/Frappe/caramel-cafe.png'  },
+        { nombre: 'Choco Menta', precio: 9.50, imagen:'./images/Frappe/choco-menta.png'  },
     ],
     'fresas': [
-        { nombre: 'chocolate', precio: 10.00, imagen:'./images/fresas/fresa-con-chocolate.png' },
-        { nombre: 'vainilla', precio: 10.00, imagen:'./images/fresas/fresa-con-vainilla.png' },
+        { nombre: 'Chocolate con Fresa', precio: 10.00, imagen:'./images/fresas/fresa-con-chocolate.png' },
+        { nombre: 'Vainilla', precio: 10.00, imagen:'./images/fresas/fresa-con-vainilla.png' },
+        { nombre: 'Fudge', precio: 10.00, imagen:'./images/fresas/fresa-con-fudge.png' },
     ],
     'sodas-heladas': [
-        { nombre: 'sky soda', precio: 7.00, imagen:'./images/sodasHeladas/sh-sky.jpg' },
-        { nombre: 'Tropical', precio:  7.00, imagen:'./images/sodasHeladas/sh-tropical.jpg'},
-        { nombre: 'Paradise', precio: 7.00, imagen:'./images/sodasHeladas/sh-paradise.jpg' },
-        { nombre: 'Galaxy', precio: 7.00, imagen:'./images/sodasHeladas/sh-galaxi.jpg' },
+        { nombre: 'Sky soda', precio: 7.00, imagen:'./images/SodasconHielo/sky.png' },
+        { nombre: 'Hawai', precio:  7.00, imagen:'./images/SodasconHielo/hawai.png'},
+        { nombre: 'Galaxy', precio: 7.00, imagen:'./images/SodasconHielo/galaxi.png' },
     ],
     'helados': [
-        { nombre: 'cono', precio: 3.00, imagen:'./images/helados/1cono.png'  },
-        { nombre: 'conazo', precio: 5.00, imagen:'./images/helados/conazo.png'  },
+        { nombre: 'Cono', precio: 2.50, imagen:'./images/helados/cono.png'  },
+        { nombre: 'Conazo', precio: 5.00, imagen:'./images/helados/conazo.png'  },
     ],
     'bebidas-frias': [
-        { nombre: 'Café Americano F', precio: 8.50, imagen:'./images/bebidasFrias/americano-frio.jpg'},
-        { nombre: 'Orange Americano ', precio: 9.50, imagen:'./images/bebidasFrias/orange-americano.jpg'},
-        { nombre: 'Capuccino F', precio: 9.50, imagen:'./images/bebidasFrias/capuccino-frio.jpg'},
-        { nombre: 'Café Mocha F', precio: 9.50, imagen:'./images/bebidasFrias/mocha-frio.jpg'},
-        { nombre: 'Rosa y Chocolate', precio: 10.50, imagen:'./images/bebidasFrias/rosa.png'},
-        { nombre: 'Matcha Milk', precio: 12.50, imagen:'./images/bebidasFrias/matcha-frio.jpg'},
-        { nombre: 'Leche c/ chocolat', precio: 9.50 , imagen:'./images/bebidasFrias/leche-con-chocolate-frio.jpg'}
+        { nombre: 'Americano Frío', precio: 8.50, imagen:'./images/bebidasFrias/americano-frio.jpg'},
+        { nombre: 'Orange Americano ', precio: 8.50, imagen:'./images/bebidasFrias/orange-americano.jpg'},
+        { nombre: 'Capuccino Frío', precio: 9.50, imagen:'./images/bebidasFrias/capuccino-frio.jpg'},
+        { nombre: 'Café Mocha Frío', precio: 9.50, imagen:'./images/bebidasFrias/moccha-frio.jpg'},
+        { nombre: 'Rosa y Chocolate', precio: 10.50, imagen:'./images/bebidasFrias/rosa-chocolate.jpeg'},
+        { nombre: 'Leche con chocolate', precio: 9.50 , imagen:'./images/bebidasFrias/leche-chocolate.jpg'},
+        { nombre: 'Café y Blueberry Milk', precio: 10.50 , imagen:'./images/bebidasFrias/Cafe-BluerryMilk.jpg'},
     ],
-    'packs-desayuno': [
-        { nombre: 'Duo chocovaso', precio: 29.00, imagen:'./images/bebidasCalientes/duo-chocovaso.jpeg'},
-        { nombre: 'Duo Cuchareable ', precio: 28.00, imagen:'./images/bebidasCalientes/duo-cuchareable.jpeg'},
-        { nombre: 'Duo fresas con vainilla', precio: 28.00, imagen:'./images/bebidasCalientes/duo-fresas-vainilla.jpeg'},
-        { nombre: 'Duo Pink Drink', precio: 28.00, imagen:'./images/bebidasCalientes/duo-pink-drink.jpeg'},
+    'duo-flores': [
+        { nombre: 'Duo chocovaso', precio: 29.00, imagen:'./images/Duoflores/duo-chocovaso.jpeg'},
+        { nombre: 'Duo Cuchareable ', precio: 28.00, imagen:'./images/Duoflores/duo-cuchareable.jpeg'},
+        { nombre: 'Duo fresas con vainilla', precio: 28.00, imagen:'./images/Duoflores/duo-fresas-vainilla.jpeg'},
+        { nombre: 'Duo Pink Drink', precio: 28.00, imagen:'./images/Duoflores/duo-pink-drink.jpeg'},
     ],
-     'Pasteleria': [
-        { nombre: 'Duo chocovaso', precio: 25.50, imagen:'./images/bebidasCalientes/duo-chocovaso.jpeg'},
-        { nombre: 'Duo Cuchareable ', precio: 9.00, imagen:'./images/bebidasCalientes/duo-cuchareable.jpeg'},
-        { nombre: 'Duo fresas con vainilla', precio: 9.50, imagen:'./images/bebidasCalientes/duo-fresas-vainilla.jpeg'},
-        { nombre: 'Duo Pink Drink', precio: 9.50, imagen:'./images/bebidasCalientes/duo-pink-drink.jpeg'},
+    'matcha': [
+        { nombre: 'Matcha Milk', precio: 12.50, imagen:'./images/Matcha/Matcha-Milk.jpg'},
+        { nombre: 'Blueberry Matcha', precio: 14.50, imagen:'./images/Matcha/Blueberry-Matcha.jpg'},
+        { nombre: 'Matcha Pink Milk', precio: 15.50, imagen:'./images/Matcha/Matcha-Pink-Milk.jpg'},
+        { nombre: 'Limonada Matcha', precio: 12.50, imagen:'./images/Matcha/Limonada-Matcha.jpg'},
+        { nombre: 'Matcha con mango', precio: 14.50, imagen:'./images/Matcha/Matcha-Mango.jpg'},
+    ],
+    'naranja-miel': [
+        { nombre: 'Naranja con Miel Caliente', precio: 11.50, imagen:'./images/NaranjaconMiel/Naranja-Miel.jpg'},
+    ],
+    'cuchareables': [
+        { nombre: 'Cuchareables (consultar sabores)', precio: 9.50, imagen:'./images/Cuchareables/Cuchareables.jpg'},
+    ],
+    'postres': [
+        { nombre: 'Torta de Chocolate', precio: 7.00, imagen:'./images/Postres/Torta-Chocolate.jpg'},
+        { nombre: 'Torta de Chocolate con Helado', precio: 8.50, imagen:'./images/Postres/Chocolate-Helado.jpg'},
+        { nombre: 'Torta del Dia (porsión)', precio: 7.00, imagen:'./images/Postres/Torta-Porcion.jpg'},
+        { nombre: 'Empanada', precio: 5.00, imagen:'./images/Postres/Empanada.jpg'},
+        { nombre: 'Sanwich, Jamón y Queso', precio: 7.00, imagen:'./images/Postres/Sandwich-Jamon-Queso.jpg'},
     ],
 };
 
@@ -158,4 +172,5 @@ function Delivery() {
     window.open(`https://www.pedidosya.com.pe/restaurantes/huancayo/organica-cafe-y-plantas-92e2f535-5f7d-4012-8a8c-471bfee516e7-menu`, '_blank');
 }   
     
+
 
